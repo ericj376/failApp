b4b246a140320e79753631fd9f4560caeced035b
AmitaPatel3 <amitapatel3@hotmail.com>
2016-04-12 13:22:51 -0600

    readd client

---
+++ client/index.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+  render: function(){
+    return(
+      <h1> Hello Fail Ap</h1>
+      )
+  }
+});
+
+ReactDOM.render( <App />, document.getElementById('app'));
