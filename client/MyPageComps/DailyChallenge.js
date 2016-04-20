56f05414f368236f63112fbf2bb9b54e774e6bcf
AmitaPatel3 <amitapatel3@hotmail.com>
2016-04-20 13:29:48 -0600

    rendered daily challenge card

--- client/MyPageComps/DailyChallenge.js
+++ client/MyPageComps/DailyChallenge.js
@@ -13,19 +13,33 @@
 
 var React = require('react');
 
+
 var DailyChallenge = React.createClass ({
   render: function() {
     return (
-      <div>
-        <div className="singlefailpost">
-          <p>{this.props.oneFail.title}</p>
-          <img src={this.props.oneFail.img}/>
-          <p>{this.props.oneFail.challenge}</p>
-          <p>{this.props.oneFail.ratings}</p>   
+      <div className="card card-size">
+        <div className="card-body"><img className="fail-img" src={this.props.oneFail.img}/>
+          <div className="card-body card-flex">
+            <div className="buttonPosition">  
+                <div>
+                  <button onClick="" className="btnSpace btn btn-primary"> View More </button>
+                </div>
+                <div className="">
+                  <p>{this.props.oneFail.title}</p>
+                </div>
+                <div className="">
+                  <p>Category: {this.props.oneFail.category.name}</p>
+                </div>
+                <div className="">
+                  <p>{this.props.oneFail.challenge}</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    )
-  }
-});
+      )        
+    }
+  });
 
 module.exports = DailyChallenge;
+
